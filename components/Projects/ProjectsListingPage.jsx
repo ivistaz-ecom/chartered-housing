f4204d978013dcbce03e998f764daa10f625de66
Shubham Shree <shubhamshree.dev@gmail.com>
2025-07-17 10:21:53 +0530

    Ongoing Project: New design updated

--- components/Projects/ProjectsListingPage.jsx
+++ components/Projects/ProjectsListingPage.jsx
@@ -7,12 +7,21 @@ const ProjectsListingPage = ({ projectsList }) => {
     <div className="my-10">
       <div className="max-w-7xl mx-auto space-y-10">
         {projectsList.map((project, i) => (
-          <div className="max-w-7xl mx-auto" key={i}>
-            <h2
+          <div className="max-w-7xl mx-auto flex justify-between" key={i}>
+            {/* <h2
               className="text-[80px] leading-none text-[#646464] font-playfair mb-10"
               dangerouslySetInnerHTML={{ __html: project.title }}
-            ></h2>
-            <div className="flex justify-end">
+            ></h2> */}
+            <div className="flex flex-col gap-4 items-start">
+              <Image
+                src={project.logoUrl}
+                width={project.width}
+                height={project.height}
+                alt={project.projectName}
+              />
+              <p className="text-3xl text-primary-gray">{project.location}</p>
+            </div>
+            <div className="flex justify-end pt-16">
               <div className="w-[800px] text-lg text-[#646464] flex flex-col gap-5">
                 <p>{project.desc1}</p>
                 <p>{project.desc2}</p>
@@ -26,10 +35,11 @@ const ProjectsListingPage = ({ projectsList }) => {
                   />
                 </div>
 
-                <h3 className="text-4xl text-[#ED1C24] font-playfair">
-                  {project.projectName}
-                </h3>
-                <p>{project.shortDesc}</p>
+                <h3
+                  className="text-3xl text-primary-red leading-relaxed"
+                  dangerouslySetInnerHTML={{ __html: project.shortDesc }}
+                ></h3>
+                {/* <p>{project.shortDesc}</p> */}
                 <Link href={`/projects/ongoing${project.path}`}>
                   <Button
                     variant="outline"
