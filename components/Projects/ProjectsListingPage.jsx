9b69494cb31fa09cd7aaf6894231c0e606f9b730
Shubham Shree <shubhamshree.dev@gmail.com>
2025-07-17 14:55:08 +0530

    Ongoing Project New Design Updated

--- components/Projects/ProjectsListingPage.jsx
+++ components/Projects/ProjectsListingPage.jsx
@@ -7,17 +7,40 @@ const ProjectsListingPage = ({ projectsList }) => {
     <div className="my-10">
       <div className="max-w-7xl mx-auto space-y-10">
         {projectsList.map((project, i) => (
-          <div className="max-w-7xl mx-auto flex justify-between" key={i}>
-            <div className="flex flex-col gap-4 items-start">
-              <Image
-                src={project.logoUrl}
-                width={project.width}
-                height={project.height}
-                alt={project.projectName}
-              />
-              <p className="text-3xl text-primary-gray">{project.location}</p>
-            </div>
-            <div className="flex justify-end pt-16">
+          <div
+            className="max-w-7xl mx-auto flex flex-col gap-5 items-start text-lg"
+            key={i}
+          >
+            <Image
+              src={project.logoUrl}
+              width={project.width}
+              height={project.height}
+              alt={project.projectName}
+            />
+            <p className="text-3xl text-primary-gray">{project.location}</p>
+            <p>{project.desc1}</p>
+            <p>{project.desc2}</p>
+            <Image
+              src={project.image}
+              width={1000}
+              height={500}
+              alt={project.projectName}
+              className="w-full"
+            />
+            <h3
+              className="text-3xl text-primary-red leading-relaxed"
+              dangerouslySetInnerHTML={{ __html: project.shortDesc }}
+            ></h3>
+            <Link href={`/projects/ongoing${project.path}`}>
+              <Button
+                variant="outline"
+                className="border-[#ec1d23] rounded-none px-4 py-1.5 text-[#ec1d23] hover:bg-[#ec1d23] hover:text-white cursor-pointer"
+              >
+                Know More
+              </Button>
+            </Link>
+
+            {/* <div className="flex justify-end pt-16">
               <div className="w-[800px] text-lg text-[#646464] flex flex-col gap-5">
                 <p>{project.desc1}</p>
                 <p>{project.desc2}</p>
@@ -44,7 +67,7 @@ const ProjectsListingPage = ({ projectsList }) => {
                   </Button>
                 </Link>
               </div>
-            </div>
+            </div> */}
           </div>
         ))}
       </div>
